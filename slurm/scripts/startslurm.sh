ddc7b61ebb38cf7262aee7142ef3acaec7f8916a
Stephen Fox <stephen.fox@nimbix.net>
2016-12-03 20:55:00 -0600

    Automatic configuration of GPU resources if present

--- slurm/scripts/startslurm.sh
+++ slurm/scripts/startslurm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+GPU_COUNT=`ls /dev |grep nvidia[0-9] | wc -l`
+
 NCORES=`cat /etc/JARVICE/cores | wc -l`
 NNODES=`cat /etc/JARVICE/nodes | wc -l`
 
@@ -18,10 +20,21 @@ for i in `cat /etc/JARVICE/nodes`; do
 done
 
 cp /opt/conf/slurm.conf.template /tmp/slurm.conf
+cp /opt/conf/gres.conf.template /tmp/gres.conf
 
 sed -i "s/{{nodes}}/${NODES}/g" /tmp/slurm.conf
 sed -i "s/{{hostname}}/${SUBMITNODE}/g" /tmp/slurm.conf
 sed -i "s/{{cores_per_node}}/${CORES_PER_NODE}/g" /tmp/slurm.conf
+if [ $GPU_COUNT -gt 0 ]; then
+    sed -i "s/{{gres_types}}/GresTypes=gpu/g" /tmp/slurm.conf
+    sed -i "s/{{gpu_options}}/Gres=gpu:${GPU_COUNT}/g" /tmp/slurm.conf
+    sed -i "s/{{gpu_count}}/${GPU_COUNT}/g" /tmp/gres.conf
+else
+    sed -i "s/{{gres_types}}//g" /tmp/slurm.conf
+    sed -i "s/{{gpu_options}}//g" /tmp/slurm.conf
+    echo "" > /tmp/gres.conf
+fi
+
 
 sudo mkdir -p /opt/slurm/etc
 sudo cp /tmp/slurm.conf /opt/slurm/etc
@@ -31,7 +44,9 @@ sudo /opt/slurm/sbin/slurmctld
 for i in `cat /etc/JARVICE/nodes`; do
     ssh $i sudo mkdir -p /opt/slurm/etc
     scp /tmp/slurm.conf $i:/tmp/slurm.conf
+    scp /tmp/gres.conf $i:/tmp/gres.conf
     ssh $i sudo cp /tmp/slurm.conf /opt/slurm/etc/slurm.conf
+    ssh $i sudo cp /tmp/gres.conf /opt/slurm/etc/gres.conf
     ssh $i sudo /etc/init.d/munge start
     ssh $i sudo /opt/slurm/sbin/slurmd
 done
